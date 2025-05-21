175819793d6bb6cc79bd70d0953704c38030ab32
ilovealt <ilovealt@163.com>
2025-05-22 07:02:13 +0800

    核心数据结构设计

--- start.h
+++ start.h
@@ -3,4 +3,27 @@
 
 #define SIZE 100
 
+// key-value 存储单元
+typedef struct kv_entry
+{
+    char *key;
+    char *value;
+    struct kv_entry *next;
+} KVEntry;
+
+// 哈希表主题
+typedef struct
+{
+    KVEntry **buckets; // 哈希表的桶数组
+    int capacity;      // 哈希表的总容量
+    int size;          // 当前存储的元素个数
+} HashTable;
+
+// 全局数据库实例
+typedef struct
+{
+    HashTable *ht;
+    int fd; // 监听socket描述符
+} SimpleDB;
+
 #endif
