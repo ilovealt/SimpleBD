dc0b8ce82548c83b7abdff87ca141ada1a586544
ilovealt <ilovealt@163.com>
2025-05-24 15:32:48 +0800

    添加了创建DB的方法

--- start.c
+++ start.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "start.h"
+
+SimpleDB *create_db(int capacity)
+{
+    SimpleDB *db = malloc(sizeof(SimpleDB));
+    db->ht = malloc(sizeof(HashTable));
+    db->ht->capacity = capacity;
+    db->ht->buckets = calloc(capacity, sizeof(KVEntry *));
+    return db;
+}
 
 int main(int argc, char const *argv[])
 {
--- start.h
+++ start.h
@@ -3,6 +3,8 @@
 
 #define SIZE 100
 
+#endif
+
 // key-value 存储单元
 typedef struct kv_entry
 {
@@ -26,4 +28,19 @@ typedef struct
     int fd; // 监听socket描述符
 } SimpleDB;
 
-#endif
+/**
+ * 创建一个新的哈希表
+ * @param capacity 哈希表的初始容量
+ * @return 返回一个指向新创建的哈希表的指针
+ */
+SimpleDB *create_db(int capacity);
+// void destroy_db(SimpleDB *db);
+// void insert(SimpleDB *db, const char *key, const char *value);
+// char* search(SimpleDB *db, const char *key);
+// void delete(SimpleDB *db, const char *key);
+// void print_db(SimpleDB *db);
+// void clear_db(SimpleDB *db);
+// void start_server(SimpleDB *db, int port);
+// void stop_server(SimpleDB *db);
+// void handle_signal(int signal);
+// void handle_client(int client_fd, SimpleDB *db);
